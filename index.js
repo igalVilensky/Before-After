0ca8dfe116900e99ba1a82a3eca36652063815bf
igalv <vilenskyigal@gmail.com>
2021-05-05 13:39:59 +0200

    new img, js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const slider = document.querySelector(".slider input");
 const dragLine = document.querySelector(".slider .drag");
+const img = document.querySelector(".images .img2");
 
 slider.oninput = () => {
   let sliderVal = slider.value;
   dragLine.style.left = sliderVal + "%";
+  img.style.width = sliderVal + "%";
 };
